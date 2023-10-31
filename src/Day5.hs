17e63fca48f8c2ec70286c942a804ded7233b140
alex800121 <alex800121@hotmail.com>
2023-10-31 10:07:48 +0800

    day9

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,24 +1,23 @@
 module Day5 where
+
 import Data.List.Split (splitOn)
-import OpCode (runSTOC, PrimOC (..), runOpCodeWith)
 import Data.Vector.Unboxed (fromList)
+import OpCode 
 
 day5 :: IO ()
 day5 = do
-  input <- map (read @Int) . splitOn "," <$> readFile "input/input5.txt"
-  let day5a = runOpCodeWith runSTOC $ PrimOC 0 (fromList input) [1] []
-      day5b = runOpCodeWith runSTOC $ PrimOC 0 (fromList input) [5] []
+  input <- readInput <$> readFile "input/input5.txt"
+  let day5a = runOpCodeWith runSTOC $ input { _input = [1] }
+      day5b = runOpCodeWith runSTOC $ input { _input = [5] }
   putStrLn
     . ("day5a: " ++)
     . show
     . head
     . _output
-    . snd
     $ day5a
   putStrLn
     . ("day5b: " ++)
     . show
     . head
     . _output
-    . snd
     $ day5b
