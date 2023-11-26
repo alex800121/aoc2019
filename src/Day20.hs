16c6fd8f119adf2b78348183160f5e6458f1dec8
alex800121 <alex800121@hotmail.com>
2023-11-27 04:46:20 +0800

    day22

--- src/Day20.hs
+++ src/Day20.hs
@@ -1,24 +1,39 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 
 module Day20 where
 
+import Data.Bifunctor (Bifunctor (..))
 import Data.Char (toLower)
+import Data.List (sort)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Maybe (fromJust, isJust)
+import qualified Data.PQueue.Prio.Min as Q
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Debug.Trace (traceShow)
 import MyLib (drawGraph, drawMap)
-import Data.List (sort)
 
 data Block a
   = Space
-  | Outer a
-  | Inner a
-  | AA
-  | ZZ
+  | Portal {_io :: PortalIO, _name :: a}
   deriving (Show, Eq, Ord)
+
+data PortalIO = Inner | Outer deriving (Show, Eq, Ord)
+
+instance Enum PortalIO where
+  fromEnum Inner = 0
+  fromEnum Outer = 1
+  toEnum i = case i `mod` 2 of
+    0 -> Inner
+    1 -> Outer
+
+type Q a = Q.MinPQueue Int a
 
 type Donut = Map Index (Block String)
 
-type Portal = Map Index (Block Index)
+type Portal = Map (Block String) [(Int, Block String)]
 
 type Index = (Int, Int)
 
@@ -34,40 +49,101 @@ readInput s = (m', p')
         | Just a <- m Map.!? (x - 2, y),
           Just b <- m Map.!? (x - 1, y),
           a /= '.' && b /= '.' ->
-            if x - 2 == 0 then Just (Outer [a, b]) else Just (Inner [a, b])
+            if x - 2 == 0 then Just (Portal Outer [a, b]) else Just (Portal Inner [a, b])
       '.'
         | Just a <- m Map.!? (x + 1, y),
           Just b <- m Map.!? (x + 2, y),
           a /= '.' && b /= '.' ->
-            if x + 2 == w - 1 then Just (Outer [a, b]) else Just (Inner [a, b])
+            if x + 2 == w - 1 then Just (Portal Outer [a, b]) else Just (Portal Inner [a, b])
       '.'
         | Just a <- m Map.!? (x, y - 2),
           Just b <- m Map.!? (x, y - 1),
           a /= '.' && b /= '.' ->
-            if y - 2 == 0 then Just (Outer [a, b]) else Just (Inner [a, b])
+            if y - 2 == 0 then Just (Portal Outer [a, b]) else Just (Portal Inner [a, b])
       '.'
         | Just a <- m Map.!? (x, y + 1),
           Just b <- m Map.!? (x, y + 2),
           a /= '.' && b /= '.' ->
-            if y + 2 == h - 1 then Just (Outer [a, b]) else Just (Inner [a, b])
+            if y + 2 == h - 1 then Just (Portal Outer [a, b]) else Just (Portal Inner [a, b])
       '.' -> Just Space
       _ -> Nothing
     m' = Map.mapMaybe id $ Map.mapWithKey f m
-    p' =
-      Map.map
-        ( \case
-            Space -> Space
-            Inner s | s == "AA" -> AA
-            Inner s | s == "ZZ" -> ZZ
-            Outer s | s == "ZZ" -> ZZ
-            Outer s | s == "AA" -> AA
-            Inner s -> Inner $ fst $ Map.findMin $ Map.filter (== Outer s) m'
-            Outer s -> Outer $ fst $ Map.findMin $ Map.filter (== Inner s) m'
-        )
-        m'
+    portals = filter ((/= Space) . snd) $ Map.toList m'
+    p' = Map.unions $ map (\(i, k) -> Map.singleton k (bfs m' Set.empty (Set.singleton (i, Space)) 0 [])) portals
+
+dijkstra :: Portal -> String -> Set String -> Q (Block String) -> Maybe Int
+dijkstra p fin visited q = case q of
+  Q.Empty -> Nothing
+  (len, Portal _ b) Q.:< q' | b == fin -> Just len
+  (len, Portal _ b) Q.:< q' | b `Set.member` visited -> dijkstra p fin visited q'
+  (len, Portal io x) Q.:< q' ->
+    let visited' = Set.insert x visited
+        next = map (first (+ (len + 1))) $ filter ((`Set.notMember` visited') . _name . snd) $ p Map.! Portal (succ io) x
+        q'' = Q.union (Q.fromList next) q'
+     in dijkstra p fin visited' q''
+
+dijkstra' :: Portal -> Portal -> String -> Set (Int, Block String) -> Q (Int, Block String) -> Maybe Int
+dijkstra' pin pout fin visited q = case q of
+  Q.Empty -> Nothing
+  (len, (_, Portal _ b)) Q.:< _ | b == fin -> Just len
+  (len, b) Q.:< q' | b `Set.member` visited -> dijkstra' pin pout fin visited q'
+  (len, b@(level, Portal io x)) Q.:< q' ->
+    let visited' = Set.insert b visited
+        p = case compare 0 level' of
+          GT -> Map.empty
+          EQ -> pout
+          _ -> pin
+        level' = level + case io of
+          Inner -> 1
+          Outer -> -1
+        next = filter ((`Set.notMember` visited') . snd) $ maybe [] (map (bimap (+ (len + 1)) (level', ))) (p Map.!? Portal (succ io) x)
+        q'' = Q.union (Q.fromList next) q'
+     in dijkstra' pin pout fin visited' q''
+
+bfs :: Donut -> Set Index -> Set (Index, Block String) -> Int -> [(Int, Block String)] -> [(Int, Block String)]
+bfs d visited next len m
+  -- \| traceShow (s) False = undefined
+  | Set.null next = m
+  | otherwise = bfs d visited' next' (len + 1) m'
+  where
+    visited' = Set.union visited $ Set.map fst next
+    (s, p) = Set.partition ((== Space) . snd) next
+    next' =
+      Set.filter ((`Set.notMember` visited') . fst)
+        . Set.unions
+        $ map
+          ( \(x, y) ->
+              Set.map fromJust
+                . Set.filter isJust
+                $ Set.map
+                  ( \(k, a) ->
+                      let k' = bimap (+ x) (+ y) k
+                       in (k',) <$> d Map.!? k'
+                  )
+                  s
+          )
+          adjacent
+    m' = map ((len , ) . snd) (Set.toList p) ++ m
+
+adjacent = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 day20 :: IO ()
 day20 = do
+  -- (donut, portal) <- readInput <$> readFile "input/test20.txt"
   (donut, portal) <- readInput <$> readFile "input/input20.txt"
-  -- putStrLn $ unlines $ drawGraph (\case ; Nothing -> ' '; Just Space -> '.'; Just (Inner c) -> toLower $ head c; Just (Outer c) -> head c) donut
-  putStrLn $ unlines $ drawGraph (\case Nothing -> ' '; Just Space -> '.'; Just (Inner c) -> 'p'; Just (Outer c) -> 'P' ; Just AA -> 'A'; Just ZZ -> 'Z') portal
+  let fin = (`notElem` ["AA", "ZZ"]) . _name
+      pin = Map.map (filter (fin . snd)) $ Map.filterWithKey (\k _ -> fin k) portal
+      fout x = _io x == Inner || _name x `elem` ["AA", "ZZ"]
+      pout = Map.filterWithKey (\k a -> fout k && not (null a)) $ Map.map (filter (fout . snd)) portal
+  -- print donut
+  -- print $ portal
+  putStrLn
+    . ("day20a: " ++)
+    . show
+    $ dijkstra portal "ZZ" (Set.singleton "AA") (Q.fromList $ portal Map.! Portal Outer "AA")
+  -- print pin
+  -- print pout
+  putStrLn
+    . ("day20b: " ++)
+    . show
+    $ dijkstra' pin pout "ZZ" (Set.singleton (0, Portal Outer "AA")) (Q.fromList $ map (fmap (0,)) $ pout Map.! Portal Outer "AA")
