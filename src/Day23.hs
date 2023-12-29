33b0326e2bfadc8c0df98864c8566e0a59e1fbc7
alex800121 <alex800121@hotmail.com>
2023-12-30 06:55:09 +0800

    day25

--- src/Day23.hs
+++ src/Day23.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Day23 where
 
 import qualified Data.DList as DL
@@ -5,9 +7,9 @@ import Data.Functor.Identity (Identity (..))
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IM
 import Data.List.Split (chunksOf)
+import Debug.Trace (traceShow)
 import MyLib ((!?))
 import OpCode
-import Debug.Trace (traceShow)
 
 type GameState = IntMap UBOC
 
@@ -25,33 +27,45 @@ collectOutput g = (outputs, g')
         $ IM.elems g
     g' = IM.map (\x -> x {_output = Identity DL.empty}) g
 
-updateInput :: IntMap [Integer] -> GameState -> GameState
-updateInput outputs =
+updateInput :: [Integer] -> IntMap [Integer] -> GameState -> GameState
+updateInput def outputs =
   IM.mapWithKey
     ( \k a -> case outputs IM.!? k of
         Just l -> a {_input = (`DL.append` DL.fromList l) <$> _input a}
-        _ -> a {_input = (`DL.append` DL.fromList [-1]) <$> _input a}
+        -- _ -> a
+        _ ->
+          a
+            { _input =
+                ( \case
+                    DL.Nil -> DL.fromList def
+                    _ -> runIdentity (_input a)
+                )
+                  <$> _input a
+            }
     )
 
 day23a :: GameState -> [Integer]
+-- day23a g | traceShow (IM.filter (/= Identity DL.empty) $ IM.map _output g) False = undefined
 day23a g = case outputs IM.!? 255 of
   Nothing -> day23a g''
   Just x -> x
   where
     (outputs, g') = collectOutput g
-    g'' = IM.map (runOpCodeWith runSTOC) $ updateInput outputs g'
+    g'' = IM.map (runOpCodeWith runSTOC) $ updateInput [-1] outputs g'
 
 day23b :: GameState -> [Integer] -> [Integer]
 day23b g nat
-  | IM.null (IM.delete 255 outputs) = traceShow nat $ day23b (updateInput (IM.singleton 0 nat) g) nat
-  -- | IM.null (IM.delete 255 outputs) = day23b (updateInput (IM.singleton 0 nat) g) nat
-  | otherwise = case outputs IM.!? 255 of
+  -- \| traceShow (nat') False = undefined
+  | IM.null outputs && g'' == g = day23b (updateInput [] (IM.singleton 0 nat) g) nat
+  | IM.null outputs = day23b g'' nat
+  | otherwise = case nat' of
+      Nothing -> day23b g'' nat
       Just x | x !? 1 == nat !? 1 -> x
       Just x -> day23b g'' x
-      Nothing -> day23b g'' nat
   where
+    nat' = reverse . take 2 . reverse <$> outputs IM.!? 255
     (outputs, g') = collectOutput g
-    g'' = IM.map (runOpCodeWith runSTOC) $ updateInput outputs g'
+    g'' = IM.map (runOpCodeWith runSTOC) $ updateInput [-1] outputs g'
 
 day23 :: IO ()
 day23 = do
@@ -67,3 +81,5 @@ day23 = do
     . show
     . (!! 1)
     $ day23b (IM.map (runOpCodeWith runSTOC) initState) []
+
+-- print $ IM.filter (/= Identity DL.empty) $ IM.map (_output . runOpCodeWith runSTOC) initState
--- src/Day25.hs
+++ src/Day25.hs
@@ -1,4 +1,91 @@
 module Day25 where
 
+import Control.Monad (forM_)
+import Control.Monad.ST (RealWorld, ST, stToIO)
+import Data.Char (chr, ord)
+import qualified Data.DList as DL
+import Data.Functor.Identity (Identity (..))
+import Data.List (subsequences)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.STRef
+import MyLib (drawASCII)
+import OpCode
+
+dropped = subsequences items
+
+items =
+  [ "manifold",
+    "whirled peas",
+    "space heater",
+    "dark matter",
+    "antenna",
+    "bowl of rice",
+    "klein bottle",
+    "spool of cat6"
+  ]
+
+command =
+  [ "east",
+    "take manifold",
+    "south",
+    "take whirled peas",
+    "north",
+    "west",
+    "south",
+    "take space heater",
+    "south",
+    "take dark matter",
+    "north",
+    "east",
+    "north",
+    "west",
+    "south",
+    "take antenna",
+    "north",
+    "east",
+    "south",
+    "east",
+    "take bowl of rice",
+    "north",
+    "take klein bottle",
+    "north",
+    "take spool of cat6",
+    "west"
+  ]
+
+initGame :: STOC s -> ST s ()
+initGame oc = do
+  runSTOC oc
+  forM_
+    command
+    ( \s -> do
+        let input = DL.fromList . (<> [10]) . map (fromIntegral . ord) $ s
+        writeSTRef (_input oc) input
+        writeSTRef (_output oc) DL.empty
+        runSTOC oc
+    )
+
+playGame :: STOC RealWorld -> IO ()
+playGame oc = do
+  runIOOC oc
+  x <- stToIO $ readSTRef (_output oc)
+  putStrLn $ map (chr . fromIntegral) $ DL.toList x
+  input <- DL.fromList . (<> [10]) . map (fromIntegral . ord) <$> getLine
+  stToIO $ writeSTRef (_input oc) input
+  stToIO $ writeSTRef (_output oc) DL.empty
+  playGame oc
+
+dropItemOutput :: UBOC -> String -> String
+dropItemOutput g item = drawASCII . DL.toList . runIdentity . _output $ runOpCodeWith runSTOC g'
+  where
+    input = DL.fromList . map (fromIntegral . ord) $ item ++ "\nnorth\n"
+    g' = g {_input = Identity input}
+
 day25 :: IO ()
-day25 = return ()
+day25 = do
+  input <- readInput <$> readFile "input/input25.txt"
+  input' <- stToIO $ ubToST input
+  stToIO $ initGame input'
+  g <- stToIO $ stToUB input'
+  undefined
