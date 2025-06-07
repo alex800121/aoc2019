008cb2025ef63dbcf9ddc401023b17148b2cd9b3
alex800121 <alex800121@hotmail.com>
2025-06-07 08:20:40 +0800

    tmp day13

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,9 +1,14 @@
 module Day13 where
 
-import Paths_AOC2019
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified as Map
 import IntCode
+import Paths_AOC2019
+import Control.Monad.ST.Strict (runST)
+import Control.Monad ((>=>))
+import Data.Functor ((<&>))
 
 day13 :: IO ()
 day13 = do
   v <- readPure <$> (getDataDir >>= readFile . (++ "/input/input13.txt"))
-  print v
+  print $ runST  (fromPure v >>= runIntCode <&> _output)
